bf31b5dab0c3828273177e6bb7db432a458c5f8d
doc HD <doc.divxm@gmail.com>
2018-03-02 11:58:50 +0300

    soong: Support building with Oracle Java 8
    Change-Id: Ib9d57c02586b6bc5cf4129bd00c70b9fd412ea26
    Signed-off-by: doc HD <doc.divxm@gmail.com>

--- ui/build/java.go
+++ ui/build/java.go
@@ -101,37 +101,34 @@ func checkJavaVersion(ctx Context, config Config) {
 		ctx.Println("Please follow the machine setup instructions at:")
 		ctx.Println("    https://source.android.com/source/initializing.html")
 		ctx.Println("***************************************************************")
-		ctx.Fatalln("stop")
 	}
 
-	if runtime.GOOS == "linux" {
+	//<!--if runtime.GOOS == "linux" {
 		// Early access builds of OpenJDK 9 do not contain the string "openjdk" in the
 		// version name. TODO(tobiast): Reconsider once the OpenJDK 9 toolchain is stable.
 		// http://b/62123342
-		if !strings.Contains(java_version, "openjdk") && !experimental_use_openjdk9 {
-			ctx.Println("*******************************************************")
-			ctx.Println("You are attempting to build with an unsupported JDK.")
-			ctx.Println()
-			ctx.Println("Only an OpenJDK based JDK is supported.")
-			ctx.Println()
-			ctx.Println("Please follow the machine setup instructions at:")
-			ctx.Println("    https://source.android.com/source/initializing.html")
-			ctx.Println("*******************************************************")
-			ctx.Fatalln("stop")
-		}
-	} else { // darwin
-		if strings.Contains(java_version, "openjdk") {
-			ctx.Println("*******************************************************")
-			ctx.Println("You are attempting to build with an unsupported JDK.")
-			ctx.Println()
-			ctx.Println("You use OpenJDK, but only Sun/Oracle JDK is supported.")
-			ctx.Println()
-			ctx.Println("Please follow the machine setup instructions at:")
-			ctx.Println("    https://source.android.com/source/initializing.html")
-			ctx.Println("*******************************************************")
-			ctx.Fatalln("stop")
-		}
-	}
+	//		if !strings.Contains(java_version, "openjdk") && !experimental_use_openjdk9 {
+	//			ctx.Println("*******************************************************")
+	//			ctx.Println("You are attempting to build with an unsupported JDK.")
+	//			ctx.Println()
+	//			ctx.Println("Only an OpenJDK based JDK is supported.")
+	//			ctx.Println()
+	//			ctx.Println("Please follow the machine setup instructions at:")
+	//			ctx.Println("    https://source.android.com/source/initializing.html")
+	//			ctx.Println("*******************************************************")
+	//		}
+	//	} else { // darwin
+	//		if strings.Contains(java_version, "openjdk") {
+	//			ctx.Println("*******************************************************")
+	//			ctx.Println("You are attempting to build with an unsupported JDK.")
+	//			ctx.Println()
+	//			ctx.Println("You use OpenJDK, but only Sun/Oracle JDK is supported.")
+	//			ctx.Println()
+	//			ctx.Println("Please follow the machine setup instructions at:")
+	//			ctx.Println("    https://source.android.com/source/initializing.html")
+	//			ctx.Println("*******************************************************")
+	//		}
+	//	}
 
 	incompatible_javac := strings.Contains(javac_version, incompatibleJavacStr)
 
@@ -158,6 +155,5 @@ func checkJavaVersion(ctx Context, config Config) {
 		ctx.Println("Please follow the machine setup instructions at:")
 		ctx.Println("    https://source.android.com/source/initializing.html")
 		ctx.Println("****************************************************************")
-		ctx.Fatalln("stop")
 	}
 }
